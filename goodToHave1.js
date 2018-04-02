797900690859907a8ae36042d085882baf30b831
Gonzalo Barco <gonzalo.barco@globant.com>
2018-04-02 10:52:15 -0300

    Rename must-have files and extracted good-to-have into files.

---
+++ goodToHave1.js
@@ -0,0 +1,29 @@
+function InitAutocomplete() {
+    // add Autocomplete suport
+    var autocomplete = new google.maps.places.Autocomplete(
+        (document.getElementById('addressInput')));
+    autocomplete.bindTo('bounds', map);
+
+    // add event to handle autocomplete
+    autocomplete.addListener('place_changed', function() {
+        infoWindow.close();
+        currentPositionMarker.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(14); // Why 14? Because it looks good.
+        }
+        showCurrentPosition(place.geometry.location);
+        currentPositionMarker.setVisible(true);
+    });
+}
