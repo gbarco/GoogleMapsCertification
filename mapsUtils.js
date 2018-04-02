6678d7d70c47029e30d6c55d4350f3100c9adaef
Diego Scheinin <dscheinin@gmail.com>
2018-04-01 22:37:40 -0300

    Add support for infoWindow

--- mapsUtils.js
+++ mapsUtils.js
@@ -3,7 +3,7 @@ function buildMap() {
         lat: -34.5681375,
         lng: -58.4544698
     };
-    /*var initialLocation;
+    var initialLocation;
     if (navigator.geolocation) {
         try {
             navigator.geolocation.getCurrentPosition(function(position) {
@@ -15,7 +15,7 @@ function buildMap() {
         }
     } else {
         initialLocation = buenosAires;
-    }*/
+    }
     initialLocation = buenosAires;
     var map =  new google.maps.Map(document.getElementById('map'), {
         center: initialLocation,
@@ -153,16 +153,17 @@ function buildMap() {
     return map;
 }
 
-function onMarkerClick(marker, map) {
-    var iw = new google.maps.InfoWindow();
-
-    iw.setContent(getInfoFromMarker(marker));
-    iw.open(map, marker);
+function onMarkerClick(event, map) {
+    infoWindow.setPosition(event.feature.getGeometry().get());
+    infoWindow.setOptions({pixelOffset: new google.maps.Size(0,-30)});
+    infoWindow.setContent(getInfoFromMarker(event));
+    infoWindow.open(map);
 }
 
 function getInfoFromMarker(marker) {
+    
     var content = '<div id="bodyContent">' +
-        '<p><b>Globant ' + marker.getPosition() + '</b></p>' +
+        '<p><b>Globant ' + marker.latLng.lat() + ' ' + marker.latLng.lng() + '</b></p>' +
         '</div>';
     return content;
 }
