a548c9b0091458deb40779f93dfefa464d5196f1
Gonzalo Barco <gonzalo.barco@globant.com>
2018-04-02 14:57:06 -0300

    Show directions from currentPosition.

--- mustHave5.js
+++ mustHave5.js
@@ -2,15 +2,16 @@ function initDirectionsDisplay() {
     directionsDisplay.setMap(null);
     directionsDisplay.setMap(map);
     directionsDisplay.setPanel(null);
-    directionsDisplay.setPanel(document.getElementById("directions"));
+    directionsDisplay.setPanel(directionsPanel);
 }
 
-function showDirectionsFromFeatureToUserLocation(feature) {
+function showDirectionsFromLocationMarker(feature) {
     initDirectionsDisplay();
+
     navigator.geolocation.getCurrentPosition(function(position) {
         var userLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
         directionsService.route({
-            origin: userLocation,
+            origin: currentPosition,
             destination: feature.getGeometry().get(),
             travelMode: 'DRIVING'
         }, function(response, status) {
