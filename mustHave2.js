4957219b484461ba7dd4ce6d64dead649ef35444
Gonzalo Barco <gonzalo.barco@globant.com>
2018-04-02 14:56:32 -0300

    Nearest distance implementation.

--- mustHave2.js
+++ mustHave2.js
@@ -19,3 +19,23 @@ function displayAllLocations() {
         };
     });
 }
+
+function displayNearestLocations() {
+    nearestLocationsPanel.innerHTML = "Nearest locations from current location:<br/>";
+    var distanceToLocation = new Array();
+    map.data.forEach(function(feature) {
+        var distance = google.maps.geometry.spherical.computeDistanceBetween(currentPosition, feature.getGeometry().get()) / 1000;
+        distanceToLocation.push({ distance: distance, feature: feature.getGeometry().get() });
+    });
+    showNearestLocations(distanceToLocation);
+}
+
+function showNearestLocations(distanceToLocation) {
+    distanceToLocation.sort(function(a, b) { return a.distance - b.distance });
+    nearestLocations = distanceToLocation.splice(0, distanceToLocation.length > 10 ? 10 : distanceToLocation.length);
+    nearestLocations.forEach(function(element) {
+        addStringToDivInnerHTML(nearestLocationsPanel,
+            "To " + element.feature.lat() + "," + element.feature.lng() + " " +
+            element.distance.toFixed(2) + "km<br/>");
+    });
+}
